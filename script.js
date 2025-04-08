884f97d2e68321bd5f9b796399e565c3056fe214
urs212 <117006797+urs212@users.noreply.github.com>
2025-04-08 21:36:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function handlePrivacy(accepted) {
   document.getElementById("privacyModal").style.display = "none";
   if (accepted) {
     // 실제 다운로드 링크
-    window.location.href = "download/myFile.zip";
+    window.location.href = "download/setup.exe";
   } else {
     alert("개인정보 제공에 동의하지 않으면 다운로드할 수 없습니다.");
   }
